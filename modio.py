34cd5946df5a811231a516989c80cc0020a0271a
Carlo Contavalli <ccontavalli@inscatolati.net>
2013-03-11 18:58:23 -0700

    Various improvements after testing, including: - bugfixes (syntax errors, mostly) - ability to mock out the SMB bus. - changed exceptions to inherit from IOError. - renamed Enable / Disable of relay object to OpenContact /   CloseContact.

--- modio.py
+++ modio.py
@@ -1,12 +1,62 @@
 """Utility functions to handle olimex mod-io from python."""
 
 import smbus
+import logging
 
-class DeviceNotFoundException(Exception):
+class DeviceNotFoundException(IOError):
   """Raised if we cannot communicate with the device."""
 
+class SMBBusNotConfiguredProperly(IOError):
+  """Raised if we can't find the propber smbbus setup."""
+
+
+class SmbBus(object):
+  """Represent an SMB bus to read / write from modio."""
+
+  def __init__(self, bus, address):
+    """Instantiates a SmbBus.
+
+    Args:
+      bus: integer, bus number, generally 0 or 1.
+      address: integer, generally 0x58, the address where
+        mod-io can be found.
+    """
+    self.address = address
+    try:
+      self.smb = smbus.SMBus(bus)
+    except IOError:
+      raise SMBBusNotConfiguredProperly(
+          "could not find files for access to SMB bus, you need to load "
+          "the proper modules")
+
+  def Write(self, key, value):
+    """Sends a request to olimex mod-io."""
+    try:
+      self.smb.write_byte_data(self.address, key, value)
+    except IOError:
+      raise DeviceNotFoundException("Could not communicate with device")
+
+
+class FakeBus(object):
+  """Emulates a SmbBus for testing purposes."""
+
+  def __init__(self, bus, address):
+    logging.warning("using fake SMB bus instead of real one")
+
+    self.bus = bus
+    self.address = address
+
+  def Write(self, key, value):
+    logging.debug("writing on bus %s, address %s, key %s, value %s",
+                  self.bus, self.address, key, value)
+
+
 class Device(object):
+  """Represents a mod-io device, allows to perform common operations."""
+
+  # Default address where mod-io can be found on the SMB bus.
   DEFAULT_ADDRESS = 0x58
+  # Bus number, you can use i2c tools to find it.
   DEFAULT_BUS = 1
 
   # Command to use to pilot relays.
@@ -15,12 +65,15 @@ class Device(object):
   # Bit value to use to enable/disable each relay.
   RELAYS = [1<<0, 1<<1, 1<<2, 1<<3]
 
-  def __init__(self, address=DEFAULT_ADDRESS, bus=DEFAULT_BUS):
-    self.address = address
-    self.bus = bus
+  def __init__(self, address=DEFAULT_ADDRESS, bus=DEFAULT_BUS, communicator=SmbBus):
+    """Constructs a device object.
 
-    self.smb = smbus.SMBus(bus)
-
+    Args:
+      address: integer, mod-io address.
+      bus: integer, SMB bus to use to communicate with mod-io.
+      communicator: SmbBus or FakeBus, for testing purposes.
+    """
+    self.communicator = communicator(bus, address)
     self.SetRelays(0)
 
   def GetRelays(self):
@@ -31,15 +84,8 @@ class Device(object):
     """Set the relay status."""
     if value < 0 or value > 0xf:
       raise ValueError("Invalid relay value: can be between 0 and 0xF")
-    self.Write(self.RELAY_COMMAND, value)
+    self.communicator.Write(self.RELAY_COMMAND, value)
     self.relay_status = value
-
-  def Write(self, key, value):
-    """Sends a request to olimex mod-io."""
-    try:
-      self.smb.write_byte_data(self.address, key, value)
-    except IOError:
-      raise DeviceNotFoundException("Could not communicate with device")
 
   def GetRelay(self, relay):
     """Returns the status of a relay.
@@ -63,8 +109,8 @@ class Device(object):
       return True
     return False
 
-  def EnableRelay(self, relay):
-    """Enable a specific relay.
+  def CloseContactRelay(self, relay):
+    """CloseContact a specific relay.
 
     Args:
       relay: int, 0 - 3, the relay to enable. Note that olimex
@@ -74,13 +120,13 @@ class Device(object):
       ValueError if an invalid relay number is passed.
     """
     try:
-      self.SetRelay(self.GetRelays() | self.RELAYS[relay])
+      self.SetRelays(self.GetRelays() | self.RELAYS[relay])
     except IndexError:
       raise ValueError(
           "Invalid relay: must be between 0 and %d", len(self.RELAYS) - 1)
 
-  def DisableRelay(self, relay):
-    """Disable a specific relay.
+  def OpenContactRelay(self, relay):
+    """OpenContact a specific relay.
 
     Args:
       relay: int, 0 - 3, the relay to enable. Note that olimex
@@ -90,7 +136,7 @@ class Device(object):
       ValueError if an invalid relay number is passed.
     """
     try:
-      self.SetRelay(self.GetRelays() & ((~self.RELAYS[relay]) & 0xf))
+      self.SetRelays(self.GetRelays() & ((~self.RELAYS[relay]) & 0xf))
     except IndexError:
       raise ValueError(
           "Invalid relay: must be between 0 and %d", len(self.RELAYS) - 1)
@@ -99,16 +145,16 @@ class Relay(object):
   """Represents a single relay, convenience wrapper around the device class."""
   def __init__(self, device, number):
     self.device = device
-    self.relay = number
+    self.number = number
 
   def Get(self):
     """Get status of this relay."""
     self.device.GetRelay(self.number)
 
-  def Disable(self):
-    """Disables this relay."""
-    self.device.DisableRelay(self.number)
+  def OpenContact(self):
+    """Disables this relay, by opening the contact."""
+    self.device.OpenContactRelay(self.number)
 
-  def Enable(self):
-    """Enables this relay."""
-    self.device.EnableRelay(self.number)
+  def CloseContact(self):
+    """Enables this relay, by closing the contact."""
+    self.device.CloseContactRelay(self.number)
